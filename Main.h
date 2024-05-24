3719b8d1c05a4db986f3bdf2709b054b3e673813
kuzmepaul <kuzmepaul@gmail.com>
2024-05-24 22:48:51 +0300

    added clearing sheet

--- Main.h
+++ Main.h
@@ -18,8 +18,13 @@ namespace WodExelSprint {
 	/// </summary>
 	public ref class Main : public System::Windows::Forms::Form
 	{
+	private: System::Windows::Forms::Button^ ClearXlsxButton;
+
+	private: System::Windows::Forms::TableLayoutPanel^ TableLayoutPanel;
+
+	private: System::Windows::Forms::Button^ ValidateXlsxButton;
 	private:
-		System::Windows::Forms::Button^ OpenXlsxButton;
+
 
 	public:
 		Main(void)
@@ -49,28 +54,72 @@ namespace WodExelSprint {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->OpenXlsxButton = (gcnew System::Windows::Forms::Button());
+			this->ValidateXlsxButton = (gcnew System::Windows::Forms::Button());
+			this->ClearXlsxButton = (gcnew System::Windows::Forms::Button());
+			this->TableLayoutPanel = (gcnew System::Windows::Forms::TableLayoutPanel());
+			this->TableLayoutPanel->SuspendLayout();
 			this->SuspendLayout();
 			// 
-			// OpenXlsxButton
+			// ValidateXlsxButton
 			// 
-			this->OpenXlsxButton->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Bottom)
+			this->ValidateXlsxButton->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Bottom)
 				| System::Windows::Forms::AnchorStyles::Left)
 				| System::Windows::Forms::AnchorStyles::Right));
-			this->OpenXlsxButton->Location = System::Drawing::Point(12, 12);
-			this->OpenXlsxButton->Name = L"OpenXlsxButton";
-			this->OpenXlsxButton->Size = System::Drawing::Size(209, 68);
-			this->OpenXlsxButton->TabIndex = 0;
-			this->OpenXlsxButton->Text = L"open xlsx";
-			this->OpenXlsxButton->UseVisualStyleBackColor = true;
-			this->OpenXlsxButton->Click += gcnew System::EventHandler(this, &Main::OpenXlsxFile);
+			this->ValidateXlsxButton->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 14, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->ValidateXlsxButton->Location = System::Drawing::Point(3, 3);
+			this->ValidateXlsxButton->Name = L"ValidateXlsxButton";
+			this->ValidateXlsxButton->Size = System::Drawing::Size(241, 88);
+			this->ValidateXlsxButton->TabIndex = 0;
+			this->ValidateXlsxButton->Text = L"validate xlsx";
+			this->ValidateXlsxButton->UseVisualStyleBackColor = true;
+			this->ValidateXlsxButton->Click += gcnew System::EventHandler(this, &Main::OpenXlsxFile);
+			// 
+			// ClearXlsxButton
+			// 
+			this->ClearXlsxButton->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Bottom)
+				| System::Windows::Forms::AnchorStyles::Left)
+				| System::Windows::Forms::AnchorStyles::Right));
+			this->ClearXlsxButton->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 14, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->ClearXlsxButton->Location = System::Drawing::Point(3, 97);
+			this->ClearXlsxButton->Name = L"ClearXlsxButton";
+			this->ClearXlsxButton->Size = System::Drawing::Size(241, 88);
+			this->ClearXlsxButton->TabIndex = 1;
+			this->ClearXlsxButton->Text = L"clear xlsx";
+			this->ClearXlsxButton->UseVisualStyleBackColor = true;
+			this->ClearXlsxButton->Click += gcnew System::EventHandler(this, &Main::ClearXlsxButton_Click);
+			// 
+			// TableLayoutPanel
+			// 
+			this->TableLayoutPanel->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Bottom)
+				| System::Windows::Forms::AnchorStyles::Left)
+				| System::Windows::Forms::AnchorStyles::Right));
+			this->TableLayoutPanel->AutoSize = true;
+			this->TableLayoutPanel->ColumnCount = 1;
+			this->TableLayoutPanel->ColumnStyles->Add((gcnew System::Windows::Forms::ColumnStyle(System::Windows::Forms::SizeType::Percent,
+				50)));
+			this->TableLayoutPanel->ColumnStyles->Add((gcnew System::Windows::Forms::ColumnStyle(System::Windows::Forms::SizeType::Percent,
+				50)));
+			this->TableLayoutPanel->Controls->Add(this->ValidateXlsxButton, 0, 0);
+			this->TableLayoutPanel->Controls->Add(this->ClearXlsxButton, 0, 1);
+			this->TableLayoutPanel->GrowStyle = System::Windows::Forms::TableLayoutPanelGrowStyle::FixedSize;
+			this->TableLayoutPanel->Location = System::Drawing::Point(12, 12);
+			this->TableLayoutPanel->Name = L"TableLayoutPanel";
+			this->TableLayoutPanel->RowCount = 2;
+			this->TableLayoutPanel->RowStyles->Add((gcnew System::Windows::Forms::RowStyle(System::Windows::Forms::SizeType::Percent, 50)));
+			this->TableLayoutPanel->RowStyles->Add((gcnew System::Windows::Forms::RowStyle(System::Windows::Forms::SizeType::Percent, 50)));
+			this->TableLayoutPanel->Size = System::Drawing::Size(247, 188);
+			this->TableLayoutPanel->TabIndex = 2;
 			// 
 			// Main
 			// 
-			this->ClientSize = System::Drawing::Size(233, 92);
-			this->Controls->Add(this->OpenXlsxButton);
+			this->ClientSize = System::Drawing::Size(271, 212);
+			this->Controls->Add(this->TableLayoutPanel);
 			this->Name = L"Main";
+			this->TableLayoutPanel->ResumeLayout(false);
 			this->ResumeLayout(false);
+			this->PerformLayout();
 
 		}
 
@@ -82,54 +131,142 @@ namespace WodExelSprint {
 		openFileDialog->FilterIndex = 1;
 		openFileDialog->RestoreDirectory = true;
 
-		if (openFileDialog->ShowDialog() == System::Windows::Forms::DialogResult::OK)
+		if (openFileDialog->ShowDialog() != System::Windows::Forms::DialogResult::OK)
+			return;
+
+		auto sheet = gcnew Sheet(openFileDialog->FileName);
+		auto worksheet = sheet->GetWorksheetsByName("Sprint")[0];
+
+		const static int VelocityCol = 3;
+		const static int AbsenceCol = 4;
+		const static int WaringCol = 6;
+
+		const static int symLen = 3;
+		static unsigned char symBytes[symLen] = { 0xE2, 0x9A, 0xA0 };
+		auto symbol = System::Text::Encoding::UTF8->GetString(symBytes, symLen);
+
+		auto inTeamRange = false;
+		auto skipFirstPerson = false;
+		for (auto r = 1; ; r++)
 		{
-			auto sheet = gcnew Sheet(openFileDialog->FileName);
-			auto worksheet = sheet->GetWorksheetsByName("Sprint")[0];
-
-			const static int VelocityCol = 3;
-			const static int AbsenceCol = 4;
-			const static int WaringCol = 6;
-
-			const static int symLen = 3;
-			static unsigned char symBytes[symLen] = { 0xE2, 0x9A, 0xA0 };
-			auto symbol = System::Text::Encoding::UTF8->GetString(symBytes, symLen);
-
-			auto inTeamRange = false;
-			auto skipFirstPerson = false;
-			for (auto r = 1; ; r++)
+			auto colB = sheet->GetStr(worksheet, r, VelocityCol);
+			if (colB == "Velocity")
 			{
-				auto colB = sheet->GetStr(worksheet, r, VelocityCol);
-				if (colB == "Velocity")
-				{
-					skipFirstPerson = true;
-					if (!inTeamRange)
-						inTeamRange = true;
-					continue;
-				}
-				if (inTeamRange && String::IsNullOrEmpty(colB))
-				{
-					inTeamRange = false;
-					break;
-				}
-				auto isNumber = System::Text::RegularExpressions::Regex::IsMatch(colB, "\\d+[\\.,]?\\d*");
-				if (!isNumber || !inTeamRange || skipFirstPerson)
-				{
-					skipFirstPerson = false;
-					continue;
-				}
-
-				auto value = System::Single::Parse(sheet->GetStr(worksheet, r, VelocityCol));
-				auto notFull = value < 9;
-				auto noReason = String::IsNullOrEmpty(sheet->GetStr(worksheet, r, AbsenceCol));
-				if (notFull && noReason)
-				{
-					sheet->SetStr(worksheet, r, WaringCol, symbol);
-				}
+				skipFirstPerson = true;
+				if (!inTeamRange)
+					inTeamRange = true;
+				continue;
+			}
+			if (inTeamRange && String::IsNullOrEmpty(colB))
+			{
+				inTeamRange = false;
+				break;
+			}
+			auto isNumber = System::Text::RegularExpressions::Regex::IsMatch(colB, "\\d+[\\.,]?\\d*");
+			if (!isNumber || !inTeamRange || skipFirstPerson)
+			{
+				skipFirstPerson = false;
+				continue;
 			}
 
-			sheet->SetVisible(true);
+			auto value = System::Single::Parse(sheet->GetStr(worksheet, r, VelocityCol));
+			auto notFull = value < 9;
+			auto noReason = String::IsNullOrEmpty(sheet->GetStr(worksheet, r, AbsenceCol));
+			if (notFull && noReason)
+			{
+				sheet->SetStr(worksheet, r, WaringCol, symbol);
+			}
 		}
+
+		sheet->SetVisible(true);
+	}
+
+	private: System::Void ClearXlsxButton_Click(System::Object^ sender, System::EventArgs^ e) {
+		OpenFileDialog^ openFileDialog = gcnew OpenFileDialog;
+
+		openFileDialog->InitialDirectory = ".";
+		openFileDialog->Filter = "xlsx files (*.xlsx)|*.xlsx|All files (*.*)|*.*";
+		openFileDialog->FilterIndex = 1;
+		openFileDialog->RestoreDirectory = true;
+
+		if (openFileDialog->ShowDialog() != System::Windows::Forms::DialogResult::OK)
+			return;
+
+		auto sheet = gcnew Sheet(openFileDialog->FileName);
+		auto worksheet = sheet->GetWorksheetsByName("Sprint")[0];
+
+		const static int VelocityCol = 3;
+		const static int AbsenceCol = 4;
+		const static int WaringCol = 6;
+
+		const static int symLen = 3;
+		static unsigned char symBytes[symLen] = { 0xE2, 0x9A, 0xA0 };
+		auto symbol = System::Text::Encoding::UTF8->GetString(symBytes, symLen);
+
+		auto inTeamRange = false;
+		auto skipFirstPerson = false;
+		for (auto r = 1; ; r++)
+		{
+			auto colB = sheet->GetStr(worksheet, r, VelocityCol);
+			if (colB == "Velocity")
+			{
+				skipFirstPerson = true;
+				if (!inTeamRange)
+					inTeamRange = true;
+				continue;
+			}
+			if (inTeamRange && String::IsNullOrEmpty(colB))
+			{
+				inTeamRange = false;
+				break;
+			}
+			auto isNumber = System::Text::RegularExpressions::Regex::IsMatch(colB, "-?\\d+[\\.,]?\\d*");
+			if (!isNumber || !inTeamRange)
+			{
+				continue;
+			}
+
+			sheet->SetStr(worksheet, r, VelocityCol, skipFirstPerson ? "0" : "9");
+			sheet->SetStr(worksheet, r, AbsenceCol, "");
+			sheet->SetStr(worksheet, r, WaringCol, "");
+
+			skipFirstPerson = false;
+		}
+
+		{
+			const int dateRow = 1;
+			const int dateStartCol = 2;
+			const int dateFinishCol = 3;
+			sheet->SetStr(worksheet, dateRow, dateStartCol, "");
+			sheet->SetStr(worksheet, dateRow, dateFinishCol, "");
+		}
+
+		auto worksheets = sheet->GetWorksheetsByName("(.*team)|(DevOps)");
+		for each (Worksheet ^ worksheet in worksheets)
+		{
+			auto name = worksheet->Name;
+			auto checkForEmptyCol = 0;
+			auto numbersVisited = 0;
+			while (numbersVisited < 3) {
+				auto str = sheet->GetStr(worksheet, 2, ++checkForEmptyCol);
+				auto isNum = System::Text::RegularExpressions::Regex::IsMatch(str, "-?\\d+[\\.,]?\\d*");
+				if (isNum)
+					numbersVisited++;
+			}
+
+			auto topRow = 4;
+			while (!String::IsNullOrEmpty(sheet->GetStr(worksheet, topRow, checkForEmptyCol))) {
+				sheet->SetStr(worksheet, topRow, 1, "");
+				sheet->SetStr(worksheet, topRow, 4, "");
+				sheet->SetStr(worksheet, topRow + 1, 4, "");
+				for (int j = 5; j <= checkForEmptyCol; j++)
+					sheet->SetStr(worksheet, topRow + 1, j, "");
+
+				topRow += 3;
+			}
+		}
+
+		sheet->SetVisible(true);
 	}
 	};
 }
